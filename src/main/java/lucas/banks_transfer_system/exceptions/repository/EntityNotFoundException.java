713b4ddffff4844f3918ed792faebee7e55d1b26
Lucas Siqueira <siqueiralpontes@gmail.com>
2024-10-12 14:44:42 +0000

    Creating custom exceptions for repository entities

---
+++ src/main/java/lucas/banks_transfer_system/exceptions/repository/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package lucas.banks_transfer_system.exceptions.repository;
+
+public class EntityNotFoundException extends Exception {
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/main/java/lucas/banks_transfer_system/exceptions/repository/TableConstraintException.java
@@ -0,0 +1,7 @@
+package lucas.banks_transfer_system.exceptions.repository;
+
+public class TableConstraintException extends Exception {
+	public TableConstraintException (String msg) {
+		super(msg);
+	}
+}
